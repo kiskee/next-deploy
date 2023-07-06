8170d19c8f99df8b5dab171b88306fa1f76b9520
danielmed <daniel.medina@talent.com>
2023-07-06 12:05:56 -0500

    adding some changes to front and the back

--- app/api/post/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse} from "next/server"
-
-
-export async function POST(req){
-    const{username} = await req.json()
-    
-    return NextResponse.json({message: "todo r"},{status: 200},{user: username})
-}
